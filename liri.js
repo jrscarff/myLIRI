cebad6848b9c808a2699cf2608b23ab5799e191e
Ryan Scarff <williamscarff@Williams-MacBook-Pro.local>
2018-02-14 16:47:46 -0800

    finished the four functionalities

--- liri.js
+++ liri.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var keys = require("./keys.js");
 
 var inquirer = require("inquirer");
+var request = require("request");
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 function appStart() {
 	inquirer
@@ -16,27 +21,28 @@ function appStart() {
 			}
 			]).then(function(inquirerResponse) {
 				if (inquirerResponse.application === "spotify-this-song") {
-					spotify();
+					spotifySong();
 				};
-				else if (inquirerResponse.application === "see-tweets") {
-					spotify();
+				if (inquirerResponse.application === "see-tweets") {
+					tweets();
 				};
-				else if (inquirerResponse.application === "Movie-Search") {
-					spotify();
+				if (inquirerResponse.application === "Movie-Search") {
+					movieSearch();
 				};
-				else if (inquirerResponse.application === "Do-what-it-says") {
-					spotify();
+				if (inquirerResponse.application === "Do-what-it-says") {
+					doIt();
 				};
 			})
 }
 
-function spotify() {
+function spotifySong() {
 	inquirer
 		.prompt([
 		{
 			type: "input",
 			message: "What song would you like information for?",
-			name: "song"
+			name: "song",
+			default: "Hey Ya"
 		},
 		{
 			type: "confirm",
@@ -47,9 +53,140 @@ function spotify() {
 		]).then(function(inquirerResponse) {
 			if (inquirerResponse.confirm) {
 				var song = inquirerResponse.song
-				console.log(song);
+				spotify.search({ type: 'track', query: song }, function(err, data) {
+  					if (err) {
+    					return console.log('Error occurred: ' + err);
+  					}
+  					for (var i = 0; i < data.tracks.items.length; i++) {
+  						console.log("Song Name: " + data.tracks.items[i].name);
+	  					console.log("Album: " + data.tracks.items[i].album.name);
+	  					console.log("Artist: " + data.tracks.items[i].artists[0].name); 
+	  					console.log("Preview URL: " + data.tracks.items[i].preview_url);
+	  					console.log("------------\n");
+  				}
+				});
+			}
+			else {
+				appStart();
 			}
 		})
+}
+
+function tweets() {
+	inquirer.prompt([
+	{
+			type: "confirm",
+			message: "Are you sure?",
+			name: "confirm",
+			default: true
+	}
+	]).then(function(response) {
+		if (response.confirm) {
+			var params = {
+				sreen_name: response.handle
+			}
+			client.get("favorites/list", params, function(err, tweets, response) {
+				if (!err) {
+					for (var i = 0; i < tweets.length; i++) {
+						console.log(tweets[i].text);
+						console.log(tweets[i].created_at);
+						console.log("------------\n");
+					}
+				}
+			})
+		}
+		else {
+			appStart();
+		}
+	})
+
+}
+
+function movieSearch() {
+	inquirer.prompt([
+	{
+		type: "input",
+		message: "What movie would you like information for?",
+		name: "title",
+		default: "Inception"
+	},
+	{
+		type: "confirm",
+		message: "Are you sure?",
+		name: "confirm",
+		default: true
+	}
+	]).then(function(response) {
+		if (response.confirm) {
+			var queryUrl = "http://www.omdbapi.com/?t=" + response.title + "&y=&plot=short&apikey=trilogy";
+			request(queryUrl, function(err, response, body) {
+				if (!err && response.statusCode === 200) {
+					console.log("\nTitle: " + JSON.parse(body).Title);
+					console.log("Release Year: " + JSON.parse(body).Year);
+					console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+					for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+						if (JSON.parse(body).Ratings[i].Source === 'Rotten Tomatoes') {
+							console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[i].Value)
+						}
+					}
+					console.log("Countries produced in: " + JSON.parse(body).Country);
+					console.log("Languages Available: " + JSON.parse(body).Language);
+					console.log("Plot: " + JSON.parse(body).Plot);
+					console.log("Actors: " + JSON.parse(body).Actors + "\n");
+				}
+			})
+		}
+		else {
+			appStart();
+		} 
+	})
+}
+
+function doIt() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+			return console.log(error)
+		}
+		var array = data.split(",");
+		if (array[0] === "spotify-this-song") {
+			var song = array[1];
+				spotify.search({ type: 'track', query: song }, function(err, data) {
+  					if (err) {
+    					return console.log('Error occurred: ' + err);
+  					}
+  					for (var i = 0; i < data.tracks.items.length; i++) {
+  						console.log("Song Name: " + data.tracks.items[i].name);
+	  					console.log("Album: " + data.tracks.items[i].album.name);
+	  					console.log("Artist: " + data.tracks.items[i].artists[0].name); 
+	  					console.log("Preview URL: " + data.tracks.items[i].preview_url);
+	  					console.log("------------\n");
+  					}
+				});
+		};
+		if (array[0] === "see-tweets") {
+			tweets();
+		};
+		if (array[0] === "Movie-Search") {		
+			var queryUrl = "http://www.omdbapi.com/?t=" + array[1] + "&y=&plot=short&apikey=trilogy";
+			request(queryUrl, function(err, response, body) {
+				if (!err && response.statusCode === 200) {
+					console.log("\nTitle: " + JSON.parse(body).Title);
+					console.log("Release Year: " + JSON.parse(body).Year);
+					console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+					for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+						if (JSON.parse(body).Ratings[i].Source === 'Rotten Tomatoes') {
+							console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[i].Value)
+						}
+					}
+					console.log("Countries produced in: " + JSON.parse(body).Country);
+					console.log("Languages Available: " + JSON.parse(body).Language);
+					console.log("Plot: " + JSON.parse(body).Plot);
+					console.log("Actors: " + JSON.parse(body).Actors + "\n");
+				}
+			})
+		};
+		
+	})
 }
 
 appStart();
